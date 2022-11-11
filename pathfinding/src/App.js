435547cc609bf1b370d91bd817f6f9f5d0cb7d64
garethwilliams90 <wgareth90@gmail.com>
2022-11-11 22:18:33 +0000

    node created

--- pathfinding/src/App.js
+++ pathfinding/src/App.js
@@ -1,9 +1,34 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import {nanoid} from "nanoid"
 
 export default function App() {
+    
+    // Define a node object and give it state
+    const [node, setNode] = useState({
+        id: nanoid(),
+        row: null,
+        col: null,
+
+        isStartNode: false,
+        isEndNode: false,
+        isWallNode: false,
+        isCurrentNode: false,
+        hasBeenChecked: false,
+        isClicked: false,
+        distance: null,
+    })
+
+    function createNode() {
+
+    }
+
+    const column = [node.id,node.id,node.id]
+    const array = [column, column, column]
+
+
     return (
         <div>
-            APP DIV
+            {array}
         </div>
     )
 }
---
+++ pathfinding/src/info.md
@@ -0,0 +1,9 @@
+// A Node is the fundamental block - a Node represents a square
+// on the grid and has many props associated with it such as...
+// id, row, column, isStart bool, isEnd bool, isWall bool, distance bool...
+
+// A column ==> array of multiple Nodes
+
+// A row is all the Nodes with the same index in their column array
+
+// Grid ==> array of columns
