b32057fa6d0bff1166b5fd82ff67ee8d34e87f63
garethwilliams90 <wgareth90@gmail.com>
2022-11-13 23:33:21 +0000

    Start and end can be set on clicks

--- pathfinding/src/App.js
+++ pathfinding/src/App.js
@@ -6,45 +6,67 @@ import './style.css'
 
 
 export default function App() {
+    const ROWS = 7
+    const COLS = 11
+
     const [nodes, setNodes] = useState([])
     const [startClicked, setStartClicked] = useState(false)
     const [endClicked, setEndClicked] = useState(false)
-
-    const ROWS = 7
-    const COLS = 11
     
+
     // On first render --> create 2D array of nodes
     useEffect(() => {
-        for(let col = 0; col < COLS; col++) {
+        for(let col = 1; col < COLS+1; col++) {
             const currentRow = []
-            for (let row = 0; row < ROWS; row++) {
-                currentRow.push([])
+            for (let row = 1; row < ROWS+1; row++) {
+                const currentNode = {
+                    col,
+                    row,
+                    id: (row) * (col),
+                    isStart: null,
+                    isEnd: null,
+                }
+                currentRow.push(currentNode)
             }
             setNodes(prevState => [...prevState, currentRow])
         }
     }, [])
 
     
-    function toggleStartClicked() {
-        setStartClicked(true)
-        console.log("STart node clicked....")
-        Node.setStartClicked(true)
-    }
+   function handleClick(node) {
+        if (!startClicked) {
+            console.log(`Setting start at ${node.id}`)
+            node.isStart = true
+            setStartClicked(prevState => !prevState)
+        }
+        else if (startClicked) {
+            console.log(`Setting end at ${node.id}`)
+            node.isEnd = true
+            setEndClicked(true)
+        }
+   }
+
+   
 
     console.log(nodes)
 
     const nodeElements = nodes.map((row, rowIdx) => {
         return (
             <div key={rowIdx}>
-                {row.map((node, nodeIdx) => 
-                <Node 
-                    key={nodeIdx}
-                    col={rowIdx}
-                    row={nodeIdx}
-                    id={nodeIdx}
-                    
-                >
-                </Node>)}
+                {row.map((node, nodeIdx) => {
+                    const {isStart, isEnd} = node
+                    return (
+                        <Node 
+                            key={nodeIdx}
+                            col={rowIdx}
+                            row={nodeIdx}
+                            id={(rowIdx+1) * (nodeIdx+1)}
+                            isStart={isStart}
+                            isEnd={isEnd}  
+                            handleClick={() => handleClick(node)}
+                        >{rowIdx+1 * nodeIdx+1}</Node>)
+                    })      
+                }
             </div>
         )
     })
--- pathfinding/src/Node.js
+++ pathfinding/src/Node.js
@@ -1,7 +1,9 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './Node.css'
 
 export default function Node(props) {
+    const [startClicked, setStartClicked] = useState(false)
+    const [endClicked, setEndClicked] = useState(false)
 
     // Node is an object with the following properties
     const [node, setNode] = useState({
@@ -9,8 +11,8 @@ export default function Node(props) {
         col: props.col,
         id: (props.row+1) * (props.col+1),
 
-        isStartNode: false,
-        isEndNode: false,
+        isStart: false,
+        isEnd: false,
         isWallNode: false,
         isCurrentNode: false,
         
@@ -20,7 +22,7 @@ export default function Node(props) {
     
 
     const styles = {
-        backgroundColor: node.isStartNode ? "green" : node.isEndNode ? "red" : "white"
+        backgroundColor: props.isStart ? "green" : props.isEnd ? "red" : "white"
     }
 
     
@@ -35,13 +37,44 @@ export default function Node(props) {
         })
     }
 
+    function handleClick() {
+        if (!startClicked && !endClicked) {
+            console.log("Setting start node...")
+            console.log(`Start: ${node.id}`)
+
+            setStartClicked(true)
+            setNode(prevState => {
+                return {
+                    ...prevState,
+                    isStartNode: !prevState.isStartNode
+                }
+            })
+        }
+        else if (startClicked && !endClicked) {
+            // setting end node
+            console.log("Setting end node...")
+            console.log(`End: ${node.id}`)
+
+            setEndClicked(true)
+            setNode(prevState => {
+                return {
+                    ...prevState,
+                    isEndNode: !prevState.isEndNode
+                }
+            })
+        }
+        
+    }
+
+    
+
     return (
         <div 
             className='node'
             style={styles}
-            onClick={toggleStartNode}
+            onClick={props.handleClick}
         >
-            {`id: ${node.id}`}
+            {`${node.id}`}
         </div>
     )
 }
