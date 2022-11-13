ef3c492abfd2b6f8c99b6beabb5bde33891135f3
garethwilliams90 <wgareth90@gmail.com>
2022-11-13 19:57:27 +0000

    select start node

--- pathfinding/src/App.js
+++ pathfinding/src/App.js
@@ -7,9 +7,11 @@ import './style.css'
 
 export default function App() {
     const [nodes, setNodes] = useState([])
+    const [startClicked, setStartClicked] = useState(false)
+    const [endClicked, setEndClicked] = useState(false)
 
-    const ROWS = 8
-    const COLS = 12
+    const ROWS = 7
+    const COLS = 11
     
     // On first render --> create 2D array of nodes
     useEffect(() => {
@@ -22,25 +24,37 @@ export default function App() {
         }
     }, [])
 
+    
+    function toggleStartClicked() {
+        setStartClicked(true)
+        console.log("STart node clicked....")
+        Node.setStartClicked(true)
+    }
+
     console.log(nodes)
+
+    const nodeElements = nodes.map((row, rowIdx) => {
+        return (
+            <div key={rowIdx}>
+                {row.map((node, nodeIdx) => 
+                <Node 
+                    key={nodeIdx}
+                    col={rowIdx}
+                    row={nodeIdx}
+                    id={nodeIdx}
+                    
+                >
+                </Node>)}
+            </div>
+        )
+    })
 
     return (
         <div>
-            <h1>App Component</h1>
+            <h1 className='title'>App Component</h1>
+       
             <div className='grid'>
-                {nodes.map((row, rowIdx) => {
-                    return (
-                        <div>
-                            {row.map((node, nodeIdx) => 
-                            <Node 
-                                key={nodeIdx}
-                            >
-                                {nodeIdx}
-                            </Node>)}
-                        </div>
-                    )
-                })}
-                <Node />
+                {nodeElements}
             </div>
             
         </div>
--- pathfinding/src/Node.js
+++ pathfinding/src/Node.js
@@ -1,14 +1,47 @@
 import React, {useState} from 'react'
 import './Node.css'
-//import {nanoid} from 'nanoid'
 
-export default function Node() {
+export default function Node(props) {
+
+    // Node is an object with the following properties
     const [node, setNode] = useState({
-        id: 1,
+        row: props.row,
+        col: props.col,
+        id: (props.row+1) * (props.col+1),
+
+        isStartNode: false,
+        isEndNode: false,
+        isWallNode: false,
+        isCurrentNode: false,
+        
+        distance: 0,
     })
 
+    
+
+    const styles = {
+        backgroundColor: node.isStartNode ? "green" : node.isEndNode ? "red" : "white"
+    }
+
+    
+
+    function toggleStartNode() {
+        console.log(`Node ${node.id} selected!`)
+        setNode(prevState => {
+            return {
+            ...prevState,
+            isStartNode: !prevState.isStartNode
+            }
+        })
+    }
+
     return (
-        <div className='node'>
+        <div 
+            className='node'
+            style={styles}
+            onClick={toggleStartNode}
+        >
+            {`id: ${node.id}`}
         </div>
     )
 }
